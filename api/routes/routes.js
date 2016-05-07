ae4a937f7fa6cfac3c6b8b873a9587b0fda2b2ca
Vincent TAING <vnctaing@vnctaingggg.local>
2016-05-07 17:58:59 +0200

    linted routes.js

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,10 +1,10 @@
 
 'use strict';
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const livereload = require('express-livereload');
 
 // Mongoose Models Imports
 const mongoose = require('mongoose');
@@ -21,88 +21,94 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected !')
+db.once('open', () => {
+  console.log('connected !');
 });
 
 
 app.get('/appraiser/:appraiserName', (req, res) => {
-  Appraiser.findOne({name: req.params.appraiserName}, {password: 0, email: 0},function (err, person) {
-    if (err) console.log(err);
-  })
+  Appraiser.findOne(
+    { name: req.params.appraiserName },
+    { password: 0, email: 0 },
+    (err, person) => {
+      if (err) console.log(err);
+    }
+  )
   .populate('appraisees')
   .exec(function (err, appraiser) {
-    appraiser 
-     ? res.json({'status': 200, appraiser})
-     : res.json({'status': 400, message: 'Did not find appraiser with these creds'})
-  })
+    appraiser
+     ? res.json({ status: 200, appraiser })
+     : res.json({ status: 400, message: 'Did not find appraiser with these creds'})
+  });
 });
 
 
-app.post('/appraiser', (req,res)=>{
+app.post('/appraiser', (req, res) => {
   const appraiserToAdd = new Appraiser({
     name: req.body.name,
     password: req.body.password,
-    email: req.body.email
-  })
+    email: req.body.email,
+  });
 
-  appraiserToAdd.save((err,appraiserToAdd) => {
-    if(err) return console.error(err)
+  appraiserToAdd.save((err, appraiserToAdd) => {
+    if(err) return console.error(err);
     console.log('Successfully Added : ');
     console.log(appraiserToAdd);
     res.json({
       status: 200,
-      appraiserName: appraiserToAdd.name
-    })
+      appraiserName: appraiserToAdd.name,
+    });
   });
 });
 
-app.post('/login', (req,res)=>{
-  Appraiser.findOne(req.body, function (err, appraiser) {
+app.post('/login', (req, res) => {
+  Appraiser.findOne(req.body, (err, appraiser) => {
     if (err) console.log(err);
   })
-  .exec(function(err,appraiser){
-    appraiser 
-     ? res.json({'status': 200, appraiser})
-     : res.json({'status': 400, message: 'Did not find appraiser with these creds'})
-  })
+  .exec((err, appraiser) => {
+    appraiser
+     ? res.json({ status: 200, appraiser })
+     : res.json({ status: 400, message: 'Did not find appraiser with these creds'})
+  });
 });
 
-app.post('/appraisee', (req,res) => {
+app.post('/appraisee', (req, res) => {
   let appraiser = null;
-  Appraiser.findOne({name: req.body.appraiser}, function (err, person) {
+  Appraiser.findOne({ name: req.body.appraiser }, (err, person) => {
     if (err) console.log(err);
-    person ? appraiser = person._id : console.log('did not found user');
+    person
+      ? appraiser = person._id
+      : console.log('did not found user');
   })
-  .then((appraiser)=> {
+  .then((appraiser) => {
     const appraiseeToAdd = new Appraisee({
-       appraiseeName: req.body.appraiseeName,
-       appraiserName: req.body.appraiser,
-       _appraiser: appraiser._id,
-       esteem: req.body.esteem,
-       description: req.body.description
+      appraiseeName: req.body.appraiseeName,
+      appraiserName: req.body.appraiser,
+      _appraiser: appraiser._id,
+      esteem: req.body.esteem,
+      description: req.body.description,
     });
 
-    return appraiseeToAdd.save(function (err, appraiseeToAdd) {
-     if (err) return console.error(err);
+    return appraiseeToAdd.save((err, appraiseeToAdd) => {
+      if (err) return console.error(err);
     });
   })
   .then((newAppraisee) => {
     Appraiser.update(
-      { _id: newAppraisee._appraiser},
-      { 
-        $push: { 
-          "appraisees": newAppraisee._id,
-        }
+      { _id: newAppraisee._appraiser },
+      {
+        $push: {
+          appraisees: newAppraisee._id,
+        },
       },
-      {upsert:true}
+      { upsert:true }
     )
     .exec();
-  })
-})
+  });
+});
 
-app.put('/appraisee/:appraiseeId', (req,res) => {
-  console.log('updating')
+app.put('/appraisee/:appraiseeId', (req, res) => {
+  console.log('updating');
 });
 
 app.listen(3000, function () {
