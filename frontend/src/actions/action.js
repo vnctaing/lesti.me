cffbf9eee4bace161ccd92cb068fb88466213377
Vincent TAING <vnctaing@vnctaingggg.local>
2016-05-07 17:44:15 +0200

    Linted action.js

--- frontend/src/actions/action.js
+++ frontend/src/actions/action.js
@@ -1,26 +1,23 @@
-import SHA256 from "js-sha256";
-import { push } from 'react-router-redux'
-import { normalize } from 'normalizr'
-import appraiserSchema  from '../schema/schemas.js'
+import SHA256 from 'js-sha256';
+import { push } from 'react-router-redux';
 
 export const APPRAISER_PROFILE_NOT_FOUND = 'APPRAISER_PROFILE_NOT_FOUND';
-export function appraiserProfileNotFound (){
-  return function(dispatch){
+export function appraiserProfileNotFound() {
+  return (dispatch) => {
     dispatch(push('/foo'));
     return {
-        type: RECEIVED_APPRAISER_PROFILE,
-        appraiser: json.appraiser
-    };     
-  }
+      type: APPRAISER_PROFILE_NOT_FOUND,
+    };
+  };
 }
 
 
 export const RECEIVED_APPRAISER_PROFILE = 'RECEIVED_APPRAISER_PROFILE';
-export function receivedAppraiserProfile (profile) {
-    return {
-        type: RECEIVED_APPRAISER_PROFILE,
-        profile
-    }
+export function receivedAppraiserProfile(profile) {
+  return {
+    type: RECEIVED_APPRAISER_PROFILE,
+    profile,
+  };
 }
 
 /**
@@ -28,206 +25,203 @@ export function receivedAppraiserProfile (profile) {
  * @type {Function}
  */
 export const REQUESTING_APPRAISER_PROFILE = 'REQUESTING_APPRAISER_PROFILE';
-export function requestingAppraiserProfile () {
+export function requestingAppraiserProfile() {
   return {
     type: REQUESTING_APPRAISER_PROFILE,
-  }
+  };
 }
 
 /**
  * Thunk action creator, in order to be able to do async actions.
  * The redux-thunk middleware let us have access the `dispatch` fn
  * Redux-thunk middleware let us use action creator that returns a function instead
- * of a plain object. Therefore, the returned function can delay the dispatched action 
+ * of a plain object. Therefore, the returned function can delay the dispatched action
  * @return {[fn]} [Promise]
  */
 export function fetchUserProfile(appraiser) {
-  return function (dispatch) {
+  return (dispatch) => {
     dispatch(requestingAppraiserProfile());
     fetch(`http://localhost:3000/appraiser/${appraiser}`)
       .then((response) => response.json())
       .then((json) => {
-        json.status === 200 
+        json.status === 200
           ? dispatch(receivedAppraiserProfile(json.appraiser))
-          : dispatch(push('/appraiser_not_found'))
-      })                
+          : dispatch(push('/appraiser_not_found'));
+      })
       .catch(e => console.log('error', e));
-  }
+  };
 }
 
 
 const ADDING_APPRAISEE = 'ADDING_APPRAISEE';
-export function addingAppraisee (formData) {
+export function addingAppraisee() {
   return {
     type: ADDING_APPRAISEE,
-  }
+  };
 }
 
-export function postingNewAppraisee(formData){
-  return function(dispatch){
+export function postingNewAppraisee(formData) {
+  return (dispatch) => {
     dispatch(addingAppraisee());
-    return fetch(`http://localhost:3000/appraisee`,{
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          appraiseeName: formData.appraiseeName,
-          esteem: formData.esteem,
-          description: formData.description,
-          list: formData.list,
-          appraiser: formData.appraiser
-        })
-      })
-    .then(response=> response.json())
-    .then(json=> console.log('json', json))
-    .catch(e=> console.log('error',e));
-  }
+    return fetch('http://localhost:3000/appraisee', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        appraiseeName: formData.appraiseeName,
+        esteem: formData.esteem,
+        description: formData.description,
+        list: formData.list,
+        appraiser: formData.appraiser,
+      }),
+    })
+    .then((response) => response.json())
+    .then((json) => console.log('json', json))
+    .catch((e) => console.log('error', e));
+  };
 }
 
 const REQUESTING_SIGN_UP = 'REQUESTING_SIGN_UP';
-export function requestingSignUp(){
-  return{
-    type: REQUESTING_SIGN_UP
-  }
+export function requestingSignUp() {
+  return {
+    type: REQUESTING_SIGN_UP,
+  };
 }
 
 const SUCCESSFULLY_SIGN_UP = 'SUCCESSFULLY_SIGN_UP';
-export function successfullySignUp () {
+export function successfullySignUp() {
   return {
-    type: SUCCESSFULLY_SIGN_UP
-  }
+    type: SUCCESSFULLY_SIGN_UP,
+  };
 }
 
 const FAILED_SIGN_UP = 'FAILED_SIGN_UP';
-export function failedSignUp(){
+export function failedSignUp() {
   return {
-    type: failedSignUp
-  }
+    type: FAILED_SIGN_UP,
+  };
 }
 
 
-export function signingUp(formData){
-  return function(dispatch){
+export function signingUp(formData) {
+  return (dispatch) => {
     dispatch(requestingSignUp());
-    fetch(`http://localhost:3000/appraiser`,{
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          'name': formData.username,
-          'password': SHA256(formData.password),
-          'email': formData.email
-        })
-      })
-    .then(response=> response.json())
-    .then(json=> {
-      if(json.status === 200)  {
+    fetch('http://localhost:3000/appraiser', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: formData.username,
+        password: SHA256(formData.password),
+        email: formData.email,
+      }),
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.status === 200) {
         dispatch(successfullySignUp());
         dispatch(push(`de/${json.appraiserName}`));
-      }else{
+      } else {
         dispatch(failedSignUp());
       }
     })
-    .catch(e=> console.log('error',e));
-  }
+    .catch((e) => console.log('error', e));
+  };
+}
+
+
+export const SUCCESSFULLY_SIGN_IN = 'SUCCESSFULLY_SIGN_IN';
+export function successfullySignIn() {
+  return {
+    type: SUCCESSFULLY_SIGN_IN,
+  };
+}
+
+export const FAILED_SIGN_IN = 'FAILED_SIGN_IN';
+export function failedSignIn() {
+  return {
+    type: FAILED_SIGN_IN,
+  };
 }
 
 
 export const REQUESTING_SIGN_IN = 'REQUESTING_SIGN_IN';
-export function signingIn(formData){
-  return function(dispatch){
+export function signingIn(formData) {
+  return (dispatch) => {
     dispatch(requestingSignUp());
-    fetch(`http://localhost:3000/login`,{
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          'name': formData.username,
-          'password': SHA256(formData.password),
-        })
-      })
+    fetch('http://localhost:3000/login', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: formData.username,
+        password: SHA256(formData.password),
+      }),
+    })
     .then(response => response.json())
     .then(json => {
-      if(json.status === 200)  {
+      if (json.status === 200) {
         dispatch(successfullySignIn());
         dispatch(push(`de/${json.appraiser.name}`));
-      }else{
+      } else {
         dispatch(failedSignIn());
       }
-     })
-    .catch(e => console.log('error',e));
-  }
-}
-
-export const SUCCESSFULLY_SIGN_IN = 'SUCCESSFULLY_SIGN_IN';
-export function successfullySignIn(){
-    return{
-      type: SUCCESSFULLY_SIGN_IN
-    }
-}
-
-
-export const FAILED_SIGN_IN = 'FAILED_SIGN_IN';
-export function failedSignIn() {
-  return{
-    type: FAILED_SIGN_IN
-  }
+    })
+    .catch(e => console.log('error', e));
+  };
 }
 
 export const OPEN_APPRAISEE_UPDATE_MODAL = 'OPEN_APPRAISEE_UPDATE_MODAL';
 export function openAppraiseeUpdateModal(appraiseeId) {
   return {
     type: OPEN_APPRAISEE_UPDATE_MODAL,
-    appraiseeId
-  }
+    appraiseeId,
+  };
 }
 
 export const CLOSE_APPRAISEE_UPDATE_MODAL = 'CLOSE_APPRAISEE_UPDATE_MODAL';
 export function closeAppraiseeUpdateModal(appraiseeId) {
   return {
     type: CLOSE_APPRAISEE_UPDATE_MODAL,
-    appraiseeId
-  }
+    appraiseeId,
+  };
 }
 
-export const UPDATING_APPRAISEE_ESTEEM = 'UPDATING_APPRAISEE_ESTEEM';
-export function updatingAppraiseeEsteem(hey) {
-  const appraisee = 'fuck';
-  return function (dispatch){
-    fetch(`http://localhost:3000/appraisee/${appraisee}`,{
-        method: 'put',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          'foo': 'bar'
-        })
-    })
-  }
-}
+// export const UPDATING_APPRAISEE_ESTEEM = 'UPDATING_APPRAISEE_ESTEEM';
+// export function updatingAppraiseeEsteem(hey) {
+//   const appraisee = 'fuck';
+//   return function (dispatch){
+//     fetch(`http://localhost:3000/appraisee/${appraisee}`,{
+//         method: 'put',
+//         headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//           'foo': 'bar'
+//         })
+//     })
+//   }
+// }
 
-export const SHOW_COMMENT_SECTION = 'SHOW_COMMENT_SECTION'
+export const SHOW_COMMENT_SECTION = 'SHOW_COMMENT_SECTION';
 export function showCommentSection(appraiseeId) {
   return {
     type: SHOW_COMMENT_SECTION,
-    appraiseeId
-  }
+    appraiseeId,
+  };
 }
 
-export const SHOW_ESTIMATION_SECTION = 'SHOW_ESTIMATION_SECTION'
+export const SHOW_ESTIMATION_SECTION = 'SHOW_ESTIMATION_SECTION';
 export function showEstimationSection(appraiseeId) {
   return {
     type: SHOW_ESTIMATION_SECTION,
-    appraiseeId
-  }
+    appraiseeId,
+  };
 }
-
-
-
