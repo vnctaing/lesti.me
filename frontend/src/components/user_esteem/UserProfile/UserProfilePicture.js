dce3677fb3dcdce623e64047de81a12167dcf500
Vincent TAING <vnctaing@vnctaingggg.local>
2016-08-24 18:47:55 +0200

    creates integration test file, adds  <Dropzone/> to the <UserProfilePicture /> ;

--- frontend/src/components/user_esteem/UserProfile/UserProfilePicture.js
+++ frontend/src/components/user_esteem/UserProfile/UserProfilePicture.js
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
 import Dropzone from 'react-dropzone';
 
-/**
-* It might be an overkill for the moment to create
-* a component for a UserProfile right, but the UserProfilePicture
-* might gain in UX complexity
-*/
-const UserProfilePicture = (props) => {
-  const centeredPicture = {
-    backgroundImage: `url(${props.profilePicture})`,
-    backgroundPosition: 'center center',
-    backgroundSize: 'cover',
-  };
-  return (
-    <div>
-      <img
-        style={centeredPicture}
-        className="img-responsive img-circle profile_picture"
-      />
-      <Dropzone>
-        <div>Changer de photo de profil</div>
-      </Dropzone>
-    </div>
-  );
-};
+export class UserProfilePicture extends Component {
+  handleSubmit(data) {
+    console.log('here is the fucking picture', data);
+  }
 
-export default UserProfilePicture;
+  render() {
+    const {
+      fields,
+      handleSubmit,
+      resetForm,
+    } = this.props;
+    const files = fields.avatar;
+
+    const centeredPicture = {
+      backgroundImage: `url(${this.props.profilePicture})`,
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+    };
+
+    const rounded = {
+      borderRadius: '100%',
+      height: '130px',
+      width: '130px',
+      border: '3px dashed white',
+      display: 'flex',
+      alignItems: 'center',
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          {this.props.profilePicture
+          ? <img
+            style={centeredPicture}
+            className="img-responsive img-circle profile_picture"
+          />
+          : <div>
+            <Dropzone
+              style={rounded}
+              accept="image/*"
+              onDrop={(filesToUpload) => {
+                this.handleSubmit(filesToUpload);
+              }}
+            >
+              <p className="text-center no-margin">Changer sa photo de profil</p>
+            </Dropzone>
+            <button className="btn btn-greenplain profile__validateBtn">
+              Valider
+            </button>
+          </div>}
+        </div>
+      </form>
+    );
+  }
+}
+
+const fields = ['avatar'];
+export default reduxForm({
+  form: 'userProfilePicture',
+  fields,
+})(UserProfilePicture);
